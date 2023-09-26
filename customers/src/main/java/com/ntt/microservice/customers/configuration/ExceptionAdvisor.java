c94ed3a586c6a6cf85733ec9c8363638cfb309a2
kenydanfer01 <kenychumacero19@gmail.com>
2023-09-26 14:25:31 -0500

    feat(customers): Add CustomerNotFoundException

--- customers/src/main/java/com/ntt/microservice/customers/configuration/ExceptionAdvisor.java
+++ customers/src/main/java/com/ntt/microservice/customers/configuration/ExceptionAdvisor.java
@@ -1,7 +1,23 @@
 package com.ntt.microservice.customers.configuration;
 
+import com.ntt.microservice.customers.service.exception.CustomerNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Collections;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionAdvisor {
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleCustomerNotFoundException() {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Collections.singletonMap(
+                        ExceptionMessage.MESSAGE_KEY.getMessage(),
+                        ExceptionMessage.CUSTOMER_NOT_FOUND.getMessage()
+                ));
+    }
 }
---
+++ customers/src/main/java/com/ntt/microservice/customers/service/exception/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ntt.microservice.customers.service.exception;
+
+public class CustomerNotFoundException extends RuntimeException{
+    public CustomerNotFoundException(){
+        super();
+    }
+}
