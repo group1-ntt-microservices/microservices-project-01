e245c4440428721dc94d0b1e858c4a91e358299f
kenydanfer01 <kenychumacero19@gmail.com>
2023-09-27 16:00:05 -0500

    feat(customers): Update ExceptionMessage

--- customers/src/main/java/com/ntt/microservice/customers/configuration/ExceptionMessage.java
+++ customers/src/main/java/com/ntt/microservice/customers/configuration/ExceptionMessage.java
@@ -3,11 +3,16 @@ package com.ntt.microservice.customers.configuration;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+/**
+ * Enum that defines exception messages.
+ */
 @AllArgsConstructor
 @Getter
 public enum ExceptionMessage {
-    MESSAGE_KEY("Error"),
-    CUSTOMER_NOT_FOUND("Customer not found");
+  MESSAGE_KEY("Error"),
+  CUSTOMER_NOT_FOUND("Customer not found"),
+  DOCUMENT_NUMBER_ALREADY_EXISTS("Document number already exists"),
+  DIFFERENT_DOCUMENT_NUMBER("Impossible to change document number");
 
-    private final String message;
+  private final String message;
 }
