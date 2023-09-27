e8a3a4d4f0aa2639802d6df36f2fca970e57c878
kenydanfer01 <kenychumacero19@gmail.com>
2023-09-27 16:19:18 -0500

    feat(customers): Update Controllers

--- customers/src/main/java/com/ntt/microservice/customers/api/controller/CustomerController.java
+++ customers/src/main/java/com/ntt/microservice/customers/api/controller/CustomerController.java
@@ -2,32 +2,54 @@ package com.ntt.microservice.customers.api.controller;
 
 import com.ntt.microservice.customers.api.dto.response.CustomerResponseDto;
 import com.ntt.microservice.customers.service.handler.CustomerHandler;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+/**
+ * Controller to get all clients and list by id or document number.
+ */
 @AllArgsConstructor
 @RestController
 public class CustomerController {
 
-    private CustomerHandler customerHandler;
+  private CustomerHandler customerHandler;
 
-    @GetMapping("/")
-    public ResponseEntity<List<CustomerResponseDto>> findAll(){
-        return ResponseEntity.ok(customerHandler.findAll());
-    }
+  /**
+   * Retrieves a list of all customers.
+   *
+   * @return ResponseEntity with the list of customers in the body.
+   */
+  @GetMapping("/")
+  public ResponseEntity<List<CustomerResponseDto>> findAll() {
+    return ResponseEntity.ok(customerHandler.findAll());
+  }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<CustomerResponseDto> findById(@PathVariable String id){
-        return ResponseEntity.ok(customerHandler.findById(id));
-    }
+  /**
+   * Finds a customer by  ID.
+   *
+   * @param id The ID of the customer to be found.
+   * @return ResponseEntity with the found customer in the body.
+   */
+  @GetMapping("/{id}")
+  public ResponseEntity<CustomerResponseDto> findById(@PathVariable String id) {
+    return ResponseEntity.ok(customerHandler.findById(id));
+  }
 
-    @GetMapping("/documentNumber/{documentNumber}")
-    public ResponseEntity<CustomerResponseDto> findByDocumentNumber(@PathVariable String documentNumber){
-        return ResponseEntity.ok(customerHandler.findByDocumentNumber(documentNumber));
-    }
+  /**
+   * Finds a customer by document number.
+   *
+   * @param documentNumber The document number of the customer to be found.
+   * @return ResponseEntity with the found customer in the body.
+   */
+  @GetMapping("/documentNumber/{documentNumber}")
+  public ResponseEntity<CustomerResponseDto> findByDocumentNumber(
+      @PathVariable String documentNumber
+  ) {
+    return ResponseEntity.ok(customerHandler.findByDocumentNumber(documentNumber));
+  }
+
 }
