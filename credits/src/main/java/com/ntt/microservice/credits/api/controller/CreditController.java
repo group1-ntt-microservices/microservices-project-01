9a98241169bcdde4fadcdeaddbed855418e63abd
kenydanfer01 <kenychumacero19@gmail.com>
2023-09-29 13:09:09 -0500

    feat(credits) Update CreditController

--- credits/src/main/java/com/ntt/microservice/credits/api/controller/CreditController.java
+++ credits/src/main/java/com/ntt/microservice/credits/api/controller/CreditController.java
@@ -7,6 +7,7 @@ import com.ntt.microservice.credits.feign.dto.CustomerDto;
 import com.ntt.microservice.credits.feign.service.CustomerFeignService;
 import com.ntt.microservice.credits.service.CreditMapper;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,84 +30,87 @@ public class CreditController {
   }
 
   @GetMapping("/{id}")
-  public ResponseEntity<Credit> findById(String id) {
-    return creditService.findById(id).map(ResponseEntity::ok)
+  public ResponseEntity<Credit> findById(@PathVariable String id) {
+   Optional<Credit> creditOptional = creditService.findById(id);
+    return creditOptional.map(ResponseEntity::ok)
         .orElseGet(() -> ResponseEntity.notFound().build());
   }
 
   @PostMapping("/")
-  public ResponseEntity<Credit> save(@RequestBody CreditRequestDto creditRequestDto) {
-    CustomerDto customer = customerFeignService.findCustomerById(creditRequestDto.getCustomerId());
-    // ERROR SI NO ENCUENTRA CLIENTE CON ESE ID
-    if (customer == null) {
-      return ResponseEntity.badRequest().build();
+  public ResponseEntity<Object> save(@RequestBody CreditRequestDto creditRequestDto) {
+    try {
+      CustomerDto customer = customerFeignService.findCustomerById(creditRequestDto.getCustomerId());
+      if (customer == null) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found: NULL");
+      }
+      if (customer.getCustomerType().equals(TYPE_PERSONAL_CUSTOMER)
+          && creditService.existsCustomerId(creditRequestDto.getCustomerId())) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Credit already exists for the same personal customer");
+      }
+      Credit credit = creditMapper.creditRequestDtoToCredit(creditRequestDto);
+      credit.setId(UUID.randomUUID().toString());
+      credit.setCustomerDocumentNumber(customer.getDocumentNumber());
+      credit.setCustomerType(customer.getCustomerType());
+      credit.setActive(true);
+      credit.setCreatedAt(new Date());
+      credit.setStatus(STATUS_CREDIT_PENDING);
+      return ResponseEntity.ok(creditService.save(credit));
+    } catch (feign.FeignException.NotFound ex) {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found: ERROR");
+    } catch (Exception ex) {
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Server Credit Error");
     }
-    // IMPOSIBLE CREAR MÁS DE UN CRÉDITO PARA CLIENTE PERSONAL, POR ESO
-    // VERIFICA SI EL CLIENTE ES PERSONAL Y SI EXISTEN CRÉDITOS CON ESE ID
-    if (customer.getCustomerType().equals(TYPE_PERSONAL_CUSTOMER)
-        && creditService.existsCustomerId(creditRequestDto.getCustomerId())) {
-      return ResponseEntity.badRequest().build();
-    }
-    Credit credit = creditMapper.creditRequestDtoToCredit(creditRequestDto);
-    credit.setId(UUID.randomUUID().toString());
-    credit.setCustomerDocumentNumber(customer.getDocumentNumber());
-    credit.setCustomerType(customer.getCustomerType());
-    credit.setActive(true);
-    credit.setCreatedAt(new Date());
-    credit.setStatus(STATUS_CREDIT_PENDING);
-    return ResponseEntity.ok(creditService.save(credit));
   }
 
   @DeleteMapping("/{id}")
-  public ResponseEntity<?> deleteById(@PathVariable String id) {
-    // ERROR SI EL CREDITO NO EXISTE
+  public ResponseEntity<Object> deleteById(@PathVariable String id) {
     Optional<Credit> credit = creditService.findById(id);
     if (!credit.isPresent()){
-      return ResponseEntity.badRequest().build();
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Credit not found");
     }
     creditService.deleteById(id);
     return ResponseEntity.ok().build();
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity<Credit> update(
+  public ResponseEntity<Object> update(
       @PathVariable String id,
       @RequestBody CreditRequestDto creditRequestDto
   ) {
     Optional<Credit> optionalCredit = creditService.findById(id);
-    // ERROR SI EL CREDITO NO EXISTE
     if (!optionalCredit.isPresent()){
-      return ResponseEntity.badRequest().build();
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Credit not found");
     }
-    // ERROR SI MONTO INTERES ES IGUAL AL PAGADO
-    if (creditRequestDto.getAmountInterest() == creditRequestDto.getAmountPaid()){
-      return ResponseEntity.badRequest().build();
+    if (creditRequestDto.getAmountInterest() < creditRequestDto.getAmountPaid()){
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Monto con Interes menor al Pagado");
     }
-    // ERROR SI LOS MONTOS DEL CREDITO NO COINCIDEN
     if (creditRequestDto.getAmountGranted() != optionalCredit.get().getAmountGranted()
     || creditRequestDto.getAmountInterest() != optionalCredit.get().getAmountInterest()){
-      return ResponseEntity.badRequest().build();
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Montos No Coinciden");
     }
     Credit credit = optionalCredit.get();
+    if (credit.getStatus() == STATUS_CREDIT_PAID){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Credit already paid");
+    }
     credit.setAmountPaid(creditRequestDto.getAmountPaid());
     credit.setId(id);
-    return ResponseEntity.ok(creditService.update(id, credit));
+    if (credit.getAmountPaid() == credit.getAmountInterest()){
+      credit.setStatus(STATUS_CREDIT_PAID);
+    }
+    return ResponseEntity.ok(creditService.save(credit));
   }
 
   @PatchMapping("/pay/{id}")
   public ResponseEntity<Credit> pay(@PathVariable String id, @RequestBody float amountPaid) {
     Optional<Credit> optionalCredit = creditService.findById(id);
-    // ERROR SI EL CREDITO NO EXISTE
     if (!optionalCredit.isPresent()){
       return ResponseEntity.badRequest().build();
     }
     Credit credit = optionalCredit.get();
-    // ERROR SI YA ESTÁ PAGADO COMPLETAMENTE
     if (credit.getStatus() == STATUS_CREDIT_PAID){
       return ResponseEntity.badRequest().build();
     }
     float montoRestante = credit.getAmountInterest() - credit.getAmountPaid();
-    // ERROR SI EL MONTO A PAGAR ES MAYOR AL MONTO RESTANTE
     if (amountPaid > montoRestante){
       return ResponseEntity.badRequest().build();
     }
