34828ddbc0a0379a3dfece2c2e81659ce3e44f8a
kenydanfer01 <kenychumacero19@gmail.com>
2023-09-28 20:08:39 -0500

    feat(credits) Add Constants

---
+++ credits/src/main/java/com/ntt/microservice/credits/configuration/Constants.java
@@ -0,0 +1,12 @@
+package com.ntt.microservice.credits.configuration;
+
+public class Constants {
+  private Constants() {
+    throw new IllegalStateException("Utility class");
+  }
+
+  public static final String TYPE_PERSONAL_CUSTOMER = "PERSONAL";
+  public static final String TYPE_BUSINESS_CUSTOMER = "BUSINESS";
+  public static final int STATUS_CREDIT_PAID = 1;
+  public static final int STATUS_CREDIT_PENDING = 0;
+}
