292715d0192cd00c2f1b2f691930dce2156c7ef8
kenydanfer01 <kenychumacero19@gmail.com>
2023-09-29 21:06:11 -0500

    chore(credits) Remove unnecessary pay method in CreditController

--- credits/src/main/java/com/ntt/microservice/credits/api/controller/CreditController.java
+++ credits/src/main/java/com/ntt/microservice/credits/api/controller/CreditController.java
@@ -1,19 +1,30 @@
 package com.ntt.microservice.credits.api.controller;
 
+import static com.ntt.microservice.credits.configuration.Constants.STATUS_CREDIT_PAID;
+import static com.ntt.microservice.credits.configuration.Constants.STATUS_CREDIT_PENDING;
+import static com.ntt.microservice.credits.configuration.Constants.TYPE_PERSONAL_CUSTOMER;
+
 import com.ntt.microservice.credits.api.dto.request.CreditRequestDto;
 import com.ntt.microservice.credits.domain.model.Credit;
 import com.ntt.microservice.credits.domain.service.CreditService;
 import com.ntt.microservice.credits.feign.dto.CustomerDto;
 import com.ntt.microservice.credits.feign.service.CustomerFeignService;
-import com.ntt.microservice.credits.service.CreditMapper;
+import com.ntt.microservice.credits.service.mapper.CreditMapper;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.*;
-
-import static com.ntt.microservice.credits.configuration.Constants.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
@@ -31,7 +42,7 @@ public class CreditController {
 
   @GetMapping("/{id}")
   public ResponseEntity<Credit> findById(@PathVariable String id) {
-   Optional<Credit> creditOptional = creditService.findById(id);
+    Optional<Credit> creditOptional = creditService.findById(id);
     return creditOptional.map(ResponseEntity::ok)
         .orElseGet(() -> ResponseEntity.notFound().build());
   }
@@ -39,13 +50,15 @@ public class CreditController {
   @PostMapping("/")
   public ResponseEntity<Object> save(@RequestBody CreditRequestDto creditRequestDto) {
     try {
-      CustomerDto customer = customerFeignService.findCustomerById(creditRequestDto.getCustomerId());
+      CustomerDto customer = customerFeignService
+          .findCustomerById(creditRequestDto.getCustomerId());
       if (customer == null) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found: NULL");
       }
       if (customer.getCustomerType().equals(TYPE_PERSONAL_CUSTOMER)
           && creditService.existsCustomerId(creditRequestDto.getCustomerId())) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Credit already exists for the same personal customer");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body("Credit already exists for the same personal customer");
       }
       Credit credit = creditMapper.creditRequestDtoToCredit(creditRequestDto);
       credit.setId(UUID.randomUUID().toString());
@@ -65,7 +78,7 @@ public class CreditController {
   @DeleteMapping("/{id}")
   public ResponseEntity<Object> deleteById(@PathVariable String id) {
     Optional<Credit> credit = creditService.findById(id);
-    if (!credit.isPresent()){
+    if (!credit.isPresent()) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Credit not found");
     }
     creditService.deleteById(id);
@@ -78,47 +91,25 @@ public class CreditController {
       @RequestBody CreditRequestDto creditRequestDto
   ) {
     Optional<Credit> optionalCredit = creditService.findById(id);
-    if (!optionalCredit.isPresent()){
+    if (!optionalCredit.isPresent()) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Credit not found");
     }
-    if (creditRequestDto.getAmountInterest() < creditRequestDto.getAmountPaid()){
+    if (creditRequestDto.getAmountInterest() < creditRequestDto.getAmountPaid()) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Monto con Interes menor al Pagado");
     }
     if (creditRequestDto.getAmountGranted() != optionalCredit.get().getAmountGranted()
-    || creditRequestDto.getAmountInterest() != optionalCredit.get().getAmountInterest()){
+        || creditRequestDto.getAmountInterest() != optionalCredit.get().getAmountInterest()) {
       return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Montos No Coinciden");
     }
     Credit credit = optionalCredit.get();
-    if (credit.getStatus() == STATUS_CREDIT_PAID){
+    if (credit.getStatus() == STATUS_CREDIT_PAID) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Credit already paid");
     }
     credit.setAmountPaid(creditRequestDto.getAmountPaid());
     credit.setId(id);
-    if (credit.getAmountPaid() == credit.getAmountInterest()){
+    if (credit.getAmountPaid() == credit.getAmountInterest()) {
       credit.setStatus(STATUS_CREDIT_PAID);
     }
     return ResponseEntity.ok(creditService.save(credit));
   }
-
-  @PatchMapping("/pay/{id}")
-  public ResponseEntity<Credit> pay(@PathVariable String id, @RequestBody float amountPaid) {
-    Optional<Credit> optionalCredit = creditService.findById(id);
-    if (!optionalCredit.isPresent()){
-      return ResponseEntity.badRequest().build();
-    }
-    Credit credit = optionalCredit.get();
-    if (credit.getStatus() == STATUS_CREDIT_PAID){
-      return ResponseEntity.badRequest().build();
-    }
-    float montoRestante = credit.getAmountInterest() - credit.getAmountPaid();
-    if (amountPaid > montoRestante){
-      return ResponseEntity.badRequest().build();
-    }
-    credit.setAmountPaid(credit.getAmountPaid() + amountPaid);
-    if (credit.getAmountPaid() == credit.getAmountInterest()){
-      credit.setStatus(STATUS_CREDIT_PAID);
-    }
-    return ResponseEntity.ok(creditService.save(credit));
-  }
-
 }
