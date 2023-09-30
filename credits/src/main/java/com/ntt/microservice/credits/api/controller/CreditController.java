514dc3e83489430ca2b6283eacc51eb10c302db8
kenydanfer01 <kenychumacero19@gmail.com>
2023-09-30 12:43:43 -0500

    feat(credits) Update CreditController

--- credits/src/main/java/com/ntt/microservice/credits/api/controller/CreditController.java
+++ credits/src/main/java/com/ntt/microservice/credits/api/controller/CreditController.java
@@ -1,21 +1,10 @@
 package com.ntt.microservice.credits.api.controller;
 
-import static com.ntt.microservice.credits.configuration.Constants.STATUS_CREDIT_PAID;
-import static com.ntt.microservice.credits.configuration.Constants.STATUS_CREDIT_PENDING;
-import static com.ntt.microservice.credits.configuration.Constants.TYPE_PERSONAL_CUSTOMER;
-
 import com.ntt.microservice.credits.api.dto.request.CreditRequestDto;
-import com.ntt.microservice.credits.domain.model.Credit;
-import com.ntt.microservice.credits.domain.service.CreditService;
-import com.ntt.microservice.credits.feign.dto.CustomerDto;
-import com.ntt.microservice.credits.feign.service.CustomerFeignService;
-import com.ntt.microservice.credits.service.mapper.CreditMapper;
-import java.util.Date;
+import com.ntt.microservice.credits.api.dto.response.CreditResponseDto;
+import com.ntt.microservice.credits.service.handler.CreditHandler;
 import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,90 +15,72 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Rest Controller for managing credit operations.
+ */
 @AllArgsConstructor
 @RestController
 @RequestMapping("/credit")
 public class CreditController {
 
-  private CreditService creditService;
-  private CustomerFeignService customerFeignService;
-  private CreditMapper creditMapper;
+  private CreditHandler creditHandler;
 
+  /**
+   * Retrieves a list of all credits.
+   *
+   * @return A ResponseEntity containing a list of credit response DTOs in the body.
+   */
   @GetMapping("/")
-  public ResponseEntity<List<Credit>> findAll() {
-    return ResponseEntity.ok(creditService.findAll());
+  public ResponseEntity<List<CreditResponseDto>> findAll() {
+    return ResponseEntity.ok(creditHandler.findAll());
   }
 
+  /**
+   * Retrieves a credit by its ID.
+   *
+   * @param id The ID of the credit.
+   * @return A ResponseEntity containing the credit response DTO in the body.
+   */
   @GetMapping("/{id}")
-  public ResponseEntity<Credit> findById(@PathVariable String id) {
-    Optional<Credit> creditOptional = creditService.findById(id);
-    return creditOptional.map(ResponseEntity::ok)
-        .orElseGet(() -> ResponseEntity.notFound().build());
+  public ResponseEntity<CreditResponseDto> findById(@PathVariable String id) {
+    return ResponseEntity.ok(creditHandler.findById(id));
   }
 
+  /**
+   * Creates a new credit.
+   *
+   * @param creditRequestDto The credit request DTO.
+   * @return A ResponseEntity containing the created credit response DTO in the body.
+   */
   @PostMapping("/")
-  public ResponseEntity<Object> save(@RequestBody CreditRequestDto creditRequestDto) {
-    try {
-      CustomerDto customer = customerFeignService
-          .findCustomerById(creditRequestDto.getCustomerId());
-      if (customer == null) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found: NULL");
-      }
-      if (customer.getCustomerType().equals(TYPE_PERSONAL_CUSTOMER)
-          && creditService.existsCustomerId(creditRequestDto.getCustomerId())) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-            .body("Credit already exists for the same personal customer");
-      }
-      Credit credit = creditMapper.creditRequestDtoToCredit(creditRequestDto);
-      credit.setId(UUID.randomUUID().toString());
-      credit.setCustomerDocumentNumber(customer.getDocumentNumber());
-      credit.setCustomerType(customer.getCustomerType());
-      credit.setActive(true);
-      credit.setCreatedAt(new Date());
-      credit.setStatus(STATUS_CREDIT_PENDING);
-      return ResponseEntity.ok(creditService.save(credit));
-    } catch (feign.FeignException.NotFound ex) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Client not found: ERROR");
-    } catch (Exception ex) {
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Server Credit Error");
-    }
+  public ResponseEntity<CreditResponseDto> save(@RequestBody CreditRequestDto creditRequestDto) {
+    return ResponseEntity.ok(creditHandler.save(creditRequestDto));
   }
 
+  /**
+   * Deletes a credit by its ID.
+   *
+   * @param id The ID of the credit to delete.
+   * @return A ResponseEntity with no content in the body if the deletion was successful.
+   */
   @DeleteMapping("/{id}")
   public ResponseEntity<Object> deleteById(@PathVariable String id) {
-    Optional<Credit> credit = creditService.findById(id);
-    if (!credit.isPresent()) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Credit not found");
-    }
-    creditService.deleteById(id);
-    return ResponseEntity.ok().build();
+    creditHandler.deleteById(id);
+    return ResponseEntity.noContent().build();
   }
 
+  /**
+   * Updates a credit by its ID.
+   *
+   * @param id               The ID of the credit to update.
+   * @param creditRequestDto The credit request DTO with updated information.
+   * @return A ResponseEntity containing the updated credit response DTO in the body.
+   */
   @PutMapping("/{id}")
-  public ResponseEntity<Object> update(
+  public ResponseEntity<CreditResponseDto> update(
       @PathVariable String id,
       @RequestBody CreditRequestDto creditRequestDto
   ) {
-    Optional<Credit> optionalCredit = creditService.findById(id);
-    if (!optionalCredit.isPresent()) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Credit not found");
-    }
-    if (creditRequestDto.getAmountInterest() < creditRequestDto.getAmountPaid()) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Monto con Interes menor al Pagado");
-    }
-    if (creditRequestDto.getAmountGranted() != optionalCredit.get().getAmountGranted()
-        || creditRequestDto.getAmountInterest() != optionalCredit.get().getAmountInterest()) {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Montos No Coinciden");
-    }
-    Credit credit = optionalCredit.get();
-    if (credit.getStatus() == STATUS_CREDIT_PAID) {
-      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Credit already paid");
-    }
-    credit.setAmountPaid(creditRequestDto.getAmountPaid());
-    credit.setId(id);
-    if (credit.getAmountPaid() == credit.getAmountInterest()) {
-      credit.setStatus(STATUS_CREDIT_PAID);
-    }
-    return ResponseEntity.ok(creditService.save(credit));
+    return ResponseEntity.ok(creditHandler.update(id, creditRequestDto));
   }
 }
